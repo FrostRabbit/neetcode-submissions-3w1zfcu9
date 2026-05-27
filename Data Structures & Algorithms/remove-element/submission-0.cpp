e47f0d14b8f5d5b7b1fa6a7d32f78b6ce7eb5ed4
FrostRabbit <68796320+FrostRabbit@users.noreply.github.com>
2026-05-27 18:09:23 +0800

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int l=0;
+        int r=nums.size()-1;
+        int res=0;
+        while(l<=r){
+            if(nums[l]==val){
+                nums[l] = nums[r];
+                r--;
+                continue;
+            }
+            res++;
+            l++;
+        }
+        return res;
+    }
+};
